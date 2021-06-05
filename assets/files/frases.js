33d86258128a97842c0f4bae95756c3347d27de1
Patrick Morais <ppternunes@gmail.com>
2021-06-05 06:11:01 -0300

    ajustes: footer e alinhamento de texto

--- assets/files/frases.js
+++
@@ -1,6 +0,0 @@
-let frases = ['"Prometo que vou tentar" tatuagem oldschool feita em apenas 2 visitas. Muito bom né?',
-'Granada e Caveira oldschool, feitas em 4 horas',
-'Todas essas tatuagens foram feitas aqui no estúdio! Ao todo foram 4 meses de trabalho com inúmeras visitas!',
-'Nosso tatuador Alex em ação!',
-'Anjo caído. Aqui não tem só oldschool não! Também temos diversos tipos de realismo!',
-'Alex fazendo a tatuagem de um Leão com caracteres romanos realista!']
